56c46ec66cd6cca546ec4f02f5e6019b522bbdc7
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-05-23 18:34:52 +0100

    Start a restify server
    Also add dependencies which will be needed
    Initialize linting with momentumft's eslint config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("I'm a JS file")
+const restify = require("restify")
+const {name, version} = require("../package.json")
+
+const server = restify.createServer({name, version})
+
+server.on("uncaughtException", (req, res, route, err) => {
+  console.error("uncaughtException", err)
+  throw err
+})
+
+server.use(restify.authorizationParser())
+server.use(restify.bodyParser({mapParams: false}))
+server.use(restify.queryParser({mapParams: false}))
+server.pre(restify.pre.sanitizePath())
+
+server.listen(10002, function() {
+  console.log("Server started listening on port 10002")
+})
