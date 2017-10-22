019bcefacf274ce0bd3991f3e242bc0bb9a997f2
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-10-22 12:16:17 +0100

    Add package updater to repo Update linting packages

--- src/init/db.js
+++ src/init/db.js
@@ -16,13 +16,11 @@ module.exports = function getDb() {
     }
 
     const uri = `mongodb://${authentication}${mongoHost}:${mongoPort}/${mongoDatabase}`
-    return mongodb.MongoClient.connect(uri, dbOpts)
-    .then((db) => {
+    return mongodb.MongoClient.connect(uri, dbOpts).then((db) => {
       console.log("Connected to db")
       dbInstance = db
       return dbInstance
-    })
-    .catch((err) => {
+    }).catch((err) => {
       console.error("Error connecting to db", err)
       process.exit(1)
     })
