c00d7b14f8d2ad12001caf497aea80cb1213450b
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-10-28 16:21:27 +0100

    Remove server related things because they're not needed

--- src/routes/ping/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function pingTestFactory(server, db) {
-  server.get("/pingTest", function(req, res) {
-    return db.collection("leagues").findOne().then(() => {
-      res.send(200)
-    })
-  })
-}
