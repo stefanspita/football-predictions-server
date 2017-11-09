dbf981d459f4f589fd680db23230035d59c9d15c
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-11-09 22:31:33 +0000

    Add report generation script with rating calculator

---
+++ src/processes/generate-report/calculate-player-ratings.js
@@ -0,0 +1,61 @@
+const {__, compose, divide, isNil, map, max, min, subtract} = require("ramda")
+const {
+  MAX_MINUTE_PER_BPS, MIN_MINUTE_PER_BPS, MAX_MINUTE_PER_POINT, MIN_MINUTE_PER_POINT,
+  BPS_WEIGHT, POINTS_WEIGHT, WEIGHT_MULTIPLIER_CURRENT_SEASON, WEIGHT_MULTIPLIER_2015,
+  WEIGHT_MULTIPLIER_2016,
+} = require("./rules")
+
+
+function calculateRating(stats, weight) {
+  if (isNil(stats)) return 0
+
+  const {minutesPlayed, points, bps} = stats
+  const minutesPerBps = compose(
+    divide(__, MAX_MINUTE_PER_BPS - MIN_MINUTE_PER_BPS),
+    subtract(MAX_MINUTE_PER_BPS),
+    max(MIN_MINUTE_PER_BPS),
+    min(MAX_MINUTE_PER_BPS)
+  )(minutesPlayed / bps)
+
+  const minutesPerPoint = compose(
+    divide(__, MAX_MINUTE_PER_POINT - MIN_MINUTE_PER_POINT),
+    subtract(MAX_MINUTE_PER_POINT),
+    max(MIN_MINUTE_PER_POINT),
+    min(MAX_MINUTE_PER_POINT)
+  )(minutesPlayed / points)
+
+  const rating = minutesPerBps * BPS_WEIGHT + minutesPerPoint * POINTS_WEIGHT
+  return rating * weight
+}
+
+function getRatings(stats_2015, stats_2016, stats_2017) {
+  const minutes_2015 = stats_2015 ?
+    stats_2015.minutesPlayed * WEIGHT_MULTIPLIER_2015 : 0
+  const minutes_2016 = stats_2016 ?
+    stats_2016.minutesPlayed * WEIGHT_MULTIPLIER_2016 : 0
+  const minutes_2017 = stats_2017 ?
+    stats_2017.minutesPlayed * WEIGHT_MULTIPLIER_CURRENT_SEASON : 0
+  const totalMinutes = minutes_2015 + minutes_2016 + minutes_2017
+
+  const weight_2015 = minutes_2015 / totalMinutes
+  const weight_2016 = minutes_2016 / totalMinutes
+  const weight_2017 = minutes_2017 / totalMinutes
+
+  return {weight_2015, weight_2016, weight_2017}
+}
+
+function calculatePlayerRating(player) {
+  const {
+    weight_2015, weight_2016, weight_2017,
+  } = getRatings(player["2015"], player["2016"], player.thisSeason)
+
+  const rating_2015 = calculateRating(player["2015"], weight_2015)
+  const rating_2016 = calculateRating(player["2016"], weight_2016)
+  const rating_2017 = calculateRating(player.thisSeason, weight_2017)
+  const rating = rating_2015 + rating_2016 + rating_2017
+  return {id: player.id, name: player.name, teamId: player.teamId, rating}
+}
+
+module.exports = function calculatePlayerRatings(players) {
+  return map(calculatePlayerRating, players)
+}
---
+++ src/processes/generate-report/index.js
@@ -0,0 +1,25 @@
+const getDb = require("../../init/db")
+const calculatePlayerRatings = require("./calculate-player-ratings")
+
+function generateReport() {
+  return getDb().then((db) => {
+    const teamsCollection = db.collection("teams")
+    const playersCollection = db.collection("players")
+    return Promise.all([
+      teamsCollection.find().project({_id: 0}).toArray(),
+      playersCollection.find().project({_id: 0}).toArray(),
+    ])
+  }).spread((teams, players) => {
+    return Promise.all([
+      calculatePlayerRatings(players),
+    ])
+  }).then(() => {
+    console.log("FINISHED COMPILING PLAYER REPORT")
+    process.exit(0)
+  }).catch((err) => {
+    console.error("ERROR OCCURRED", err)
+    process.exit(1)
+  })
+}
+
+generateReport()
