6e5716254e0cb8605d85e2811c7af7f9ce6035f1
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-06 12:11:55 +0000

    Slight change of rules on fixture difficulty so its weight is decreased in the overall player grade

--- src/processes/generate-report/rules.js
+++ src/processes/generate-report/rules.js
@@ -20,7 +20,7 @@ module.exports = {
   WEIGHT_CONFIDENCE_2016: 0.2,
   WEIGHT_CONFIDENCE_2015: 0.1,
 
-  FIXTURE_DIFFICULTY_GROUPS: [{value: 2, points: 3}, {value: 3, points: 2}, {value: 4, points: 1}, {value: 5, points: 0}],
+  FIXTURE_DIFFICULTY_GROUPS: [{value: 1.5, points: 3}, {value: 3, points: 2}, {value: 4, points: 1}, {value: 5, points: 0}],
   PRICE_GROUPS: [{value: 5, points: 5}, {value: 6.5, points: 4}, {value: 8, points: 3}, {value: 10, points: 2}, {value: 12, points: 1}, {value: 15, points: 0}],
   RATING_GROUPS: [{value: 85, points: 5}, {value: 75, points: 4}, {value: 65, points: 3}, {value: 55, points: 2}, {value: 40, points: 1}, {value: 0, points: 0}],
   PLAYING_CHANCE_GROUPS: [{value: 80, points: 5}, {value: 70, points: 4}, {value: 60, points: 3}, {value: 50, points: 2}, {value: 40, points: 1}, {value: 0, points: 0}],
