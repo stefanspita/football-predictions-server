c2819799b4ec4490aa0226aa0aa8fbfa27c93a6f
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-06 12:10:41 +0000

    Start calculating nextFixture difficulty

--- src/processes/generate-report/calculate-fixtrues-difficulty.js
+++ src/processes/generate-report/calculate-fixtrues-difficulty.js
@@ -1,15 +1,18 @@
-const {compose, sum, take} = require("ramda")
+const {compose, head, sum, take} = require("ramda")
 const {FIXTURE_DIFFICULTY_GROUPS} = require("./rules")
 const {findGradeAscending} = require("./utils")
 
 module.exports = function calculateFixturesDifficulty(player, team) {
+  const fixturesDifficulty_1 = head(team.fixtures)
   const fixturesDifficulty_3 = compose(sum, take(3))(team.fixtures)
   const fixturesDifficulty_5 = sum(team.fixtures)
 
+  const fixtureDifficulty_1_grade = findGradeAscending(fixturesDifficulty_1, FIXTURE_DIFFICULTY_GROUPS)
   const fixtureDifficulty_3_grade = findGradeAscending(fixturesDifficulty_3 / 3, FIXTURE_DIFFICULTY_GROUPS)
   const fixtureDifficulty_5_grade = findGradeAscending(fixturesDifficulty_5 / 5, FIXTURE_DIFFICULTY_GROUPS)
 
   return {
+    fixtureDifficulty_1_grade,
     fixtureDifficulty_3_grade,
     fixtureDifficulty_5_grade,
   }
