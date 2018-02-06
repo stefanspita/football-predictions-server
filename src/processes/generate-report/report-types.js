6dfc08be26f8e7e795da232cf38cad584d685f48
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-06 12:11:16 +0000

    Add fields property to report-types

--- src/processes/generate-report/report-types.js
+++ src/processes/generate-report/report-types.js
@@ -1,7 +1,9 @@
+const baseFields = ["name", "price", "position", "owned", "overallRating", "grade"]
+
 module.exports = [
-  {filter: {owned: true}, fileName: "my-team"},
-  {filter: {position: "gk"}, fileName: "gk"},
-  {filter: {position: "def"}, fileName: "def"},
-  {filter: {position: "mid"}, fileName: "mid"},
-  {filter: {position: "fwd"}, fileName: "fwd"},
+  {filter: {owned: true}, fields: ["name", "price", "position", "grade", "overallRating", "rating", "nextGameGrade"], fileName: "my-team"},
+  {filter: {position: "gk"}, fields: baseFields, fileName: "gk"},
+  {filter: {position: "def"}, fields: baseFields,  fileName: "def"},
+  {filter: {position: "mid"}, fields: baseFields,  fileName: "mid"},
+  {filter: {position: "fwd"}, fields: baseFields,  fileName: "fwd"},
 ]
