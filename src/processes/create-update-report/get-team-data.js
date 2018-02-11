a1e9b4e1aed70304ffad544ed6760bb2f3fbdb01
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-11 16:58:59 +0000

    Fix early console.log being triggered before the action is finished

--- src/processes/create-update-report/get-team-data.js
+++ src/processes/create-update-report/get-team-data.js
@@ -1,8 +1,9 @@
+const {tap} = require("ramda")
 const {getListOfTeamFixtures} = require("../../services/website/methods")
 
 function getTeamData(lastUpdatedGw, team) {
   return getListOfTeamFixtures(team.id)
-    .then((console.log(`Fetched team update for ${team.name}`)))
+    .then(tap(() => console.log(`Fetched team update for ${team.name}`)))
     .then((fixtures) => ({
       id: team.id,
       name: team.name,
