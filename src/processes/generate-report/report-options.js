2f13c5fba05b6e67837fd3b6bc93c8179269127c
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-17 16:15:32 +0000

    Fix report player filters

--- src/processes/generate-report/report-options.js
+++ src/processes/generate-report/report-options.js
@@ -2,8 +2,8 @@ const baseFields = ["name", "price", "position", "owned", "overallRating", "grad
 
 module.exports = [
   {filter: {owned: true}, fields: ["name", "price", "position", "grade", "overallRating", "rating", "nextGameGrade"], fileName: "my-team"},
-  {filter: {position: "gk"}, fields: baseFields, fileName: "gk"},
-  {filter: {position: "def"}, fields: baseFields,  fileName: "def"},
-  {filter: {position: "mid"}, fields: baseFields,  fileName: "mid"},
-  {filter: {position: "fwd"}, fields: baseFields,  fileName: "fwd"},
+  {filter: {position: "Goalkeeper"}, fields: baseFields, fileName: "gk"},
+  {filter: {position: "Defender"}, fields: baseFields,  fileName: "def"},
+  {filter: {position: "Midfielder"}, fields: baseFields,  fileName: "mid"},
+  {filter: {position: "Forward"}, fields: baseFields,  fileName: "fwd"},
 ]
