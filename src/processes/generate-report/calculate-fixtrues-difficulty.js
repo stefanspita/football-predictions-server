80d2475380150cdd3c454b926d79b138d13bffe9
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-17 16:09:40 +0000

    Refactor report generation code to amke it more assertive

--- src/processes/generate-report/calculate-fixtrues-difficulty.js
+++ src/processes/generate-report/calculate-fixtrues-difficulty.js
@@ -2,10 +2,10 @@ const {compose, head, sum, take} = require("ramda")
 const {FIXTURE_DIFFICULTY_GROUPS} = require("./rules")
 const {findGradeAscending} = require("./utils")
 
-module.exports = function calculateFixturesDifficulty(player, team) {
-  const fixturesDifficulty_1 = head(team.fixtures)
-  const fixturesDifficulty_3 = compose(sum, take(3))(team.fixtures)
-  const fixturesDifficulty_5 = sum(team.fixtures)
+module.exports = function calculateFixturesDifficulty(player, {fixtures}) {
+  const fixturesDifficulty_1 = head(fixtures)
+  const fixturesDifficulty_3 = compose(sum, take(3))(fixtures)
+  const fixturesDifficulty_5 = sum(fixtures)
 
   const fixtureDifficulty_1_grade = findGradeAscending(fixturesDifficulty_1, FIXTURE_DIFFICULTY_GROUPS)
   const fixtureDifficulty_3_grade = findGradeAscending(fixturesDifficulty_3 / 3, FIXTURE_DIFFICULTY_GROUPS)
--- src/processes/generate-report/calculate-price-grade.js
+++ src/processes/generate-report/calculate-price-grade.js
@@ -1,8 +1,8 @@
 const {PRICE_GROUPS} = require("./rules")
 const {findGradeAscending} = require("./utils")
 
-module.exports = function calculateFixturesDifficulty(player) {
-  const price_grade = findGradeAscending(player.price, PRICE_GROUPS)
+module.exports = function calculateFixturesDifficulty({price}) {
+  const price_grade = findGradeAscending(price, PRICE_GROUPS)
 
   return {price_grade}
 }
