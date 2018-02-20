5317cdb92e59f49befe60c90729afb6517ee9553
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-02-20 22:15:00 +0000

    Fix code I just broke

--- src/processes/fetch-all/get-team-data.js
+++ src/processes/fetch-all/get-team-data.js
@@ -1,8 +1,9 @@
+const {tap} = require("ramda")
 const {getListOfTeamFixtures} = require("../../services/website")
 
 function getTeamData(team) {
   return getListOfTeamFixtures(team.id)
-    .tap(() => console.log(`Fetched team update for ${team.name}`))
+    .then(tap(() => console.log(`Fetched team update for ${team.name}`)))
     .then((fixtures) => ({
       id: team.id,
       name: team.name,
