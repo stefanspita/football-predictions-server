b54a5a9f64c79ead66c27f316528d35e7bf7d369
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-04-07 18:43:54 +0100

    Remove unused grade for 5 fixtures

--- src/processes/generate-report/calculate-fixtrues-difficulty.js
+++ src/processes/generate-report/calculate-fixtrues-difficulty.js
@@ -5,15 +5,12 @@ const {findGradeAscending} = require("./utils")
 module.exports = function calculateFixturesDifficulty(player, {fixtures}) {
   const fixturesDifficulty_1 = head(fixtures)
   const fixturesDifficulty_3 = compose(sum, take(3))(fixtures)
-  const fixturesDifficulty_5 = sum(fixtures)
 
   const fixtureDifficulty_1_grade = findGradeAscending(fixturesDifficulty_1, FIXTURE_DIFFICULTY_GROUPS)
   const fixtureDifficulty_3_grade = findGradeAscending(fixturesDifficulty_3 / 3, FIXTURE_DIFFICULTY_GROUPS)
-  const fixtureDifficulty_5_grade = findGradeAscending(fixturesDifficulty_5 / 5, FIXTURE_DIFFICULTY_GROUPS)
 
   return {
     fixtureDifficulty_1_grade,
     fixtureDifficulty_3_grade,
-    fixtureDifficulty_5_grade,
   }
 }
