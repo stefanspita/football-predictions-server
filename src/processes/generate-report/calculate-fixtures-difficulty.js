d046f8eef76a72d51ca5c68417ed9be0b141f82f
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-08-05 16:45:20 +0100

    Fix fixture difficulty calculator

--- src/processes/generate-report/calculate-fixtures-difficulty.js
+++ src/processes/generate-report/calculate-fixtures-difficulty.js
@@ -1,4 +1,4 @@
-const {__, compose, contains, divide, filter, multiply, pluck, sum} = require("ramda")
+const {__, compose, contains, defaultTo, divide, filter, multiply, pluck, sum} = require("ramda")
 const {FIXTURE_DIFFICULTY_GROUPS} = require("./rules")
 const {findGradeAscending} = require("./utils")
 
@@ -7,16 +7,17 @@ function getFixturesGrade(fixtures, roundNumbers) {
   const relevantFixtures = filter(({round}) => contains(round, roundNumbers), fixtures)
 
   return compose(
+    defaultTo(0),
     multiply(relevantFixtures.length / numberOfRounds),
     (averageDifficulty) => findGradeAscending(averageDifficulty, FIXTURE_DIFFICULTY_GROUPS),
-    divide(__, fixtures.length),
+    divide(__, relevantFixtures.length),
     sum,
     pluck("difficulty"),
   )(relevantFixtures)
 }
 
 
-module.exports = function calculateFixturesDifficulty(player, {fixtures, lastUpdatedGameweek}) {
+module.exports = function calculateFixturesDifficulty(player, {fixtures, lastUpdatedGameweek = 0}) {
   const nextRound = lastUpdatedGameweek + 1
   const next3Rounds = [lastUpdatedGameweek + 1, lastUpdatedGameweek + 2, lastUpdatedGameweek + 3]
 
