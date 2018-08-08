c61a235a43caa25e58ec6f953646d5f34482320c
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2018-08-08 14:04:22 +0100

    Change the selection percentage rule to give better players o more selected players

--- src/processes/generate-report/calculate-selection-percent-grade.js
+++ src/processes/generate-report/calculate-selection-percent-grade.js
@@ -1,8 +1,8 @@
 const {SELECTION_PERCENTAGE_GROUPS} = require("./rules")
-const {findGradeAscending} = require("./utils")
+const {findGradeDescending} = require("./utils")
 
 module.exports = function calculateFixturesDifficulty({selectionPercent}) {
-  const selection_percent_grade = findGradeAscending(selectionPercent, SELECTION_PERCENTAGE_GROUPS)
+  const selection_percent_grade = findGradeDescending(selectionPercent, SELECTION_PERCENTAGE_GROUPS)
 
   return {selection_percent_grade}
 }
