f771f11856d83a1415050f8b008651b6c88e2ba4
Stefan <stefanspita@users.noreply.github.com>
2020-09-25 22:20:08 +0100

    Fix missing gameweek saved on a team when fetching all rounds

--- src/processes/fetch-all/get-team-data.js
+++ src/processes/fetch-all/get-team-data.js
@@ -1,15 +1,26 @@
 const Promise = require("bluebird")
+const R = require("ramda")
 const {statisticsPage, playerDetailsModal} = require("../../services/website")
+
+const getLastRound = R.compose(
+  R.dec,
+  R.reduce(R.min, 38),
+  R.pluck("round"),
+)
 
 function getTeamData(session, teams) {
   return Promise.mapSeries(teams, (team) => {
     return statisticsPage.openPlayerDetailModal(session, team.id, 0)
       .then(() => playerDetailsModal.getListOfTeamFixtures(session))
-      .then((fixtures) => ({
-        id: team.id,
-        name: team.name,
-        fixtures,
-      }))
+      .then((fixtures) => {
+        const lastUpdatedGameweek = getLastRound(fixtures)
+        return {
+          id: team.id,
+          name: team.name,
+          fixtures,
+          lastUpdatedGameweek,
+        }
+      })
       .tap(() => console.log(`Fetched team update for ${team.name}`))
       .tap(() => playerDetailsModal.exitPlayerModal(session))
       .catch((err) => {
