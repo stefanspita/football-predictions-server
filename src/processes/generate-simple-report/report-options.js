c7fd9a6e03f69d3d2a3ea12eb2dc7fe10a508071
Stefan <stefanspita@users.noreply.github.com>
2020-12-11 18:15:02 +0000

    Add my team to the simple report

--- src/processes/generate-simple-report/report-options.js
+++ src/processes/generate-simple-report/report-options.js
@@ -1,6 +1,7 @@
 const baseFields = ["name", "position", "owned", "price", "pointsPerGame"]
 
 module.exports = [
+  {filter: {owned: true}, fields: baseFields, fileName: "my-team"},
   {filter: {position: "Goalkeeper"}, fields: baseFields, fileName: "gk"},
   {filter: {position: "Defender"}, fields: baseFields,  fileName: "def"},
   {filter: {position: "Midfielder"}, fields: baseFields,  fileName: "mid"},
